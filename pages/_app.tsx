c99ae69fe95be2960048425f9d8e5f8d05014c4e
Shail Gautam <rayskcoc@gmail.com>
2022-11-11 22:13:37 +0530

    🔼 updated: wrapped app with session and recoil

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import type { Session } from 'next-auth'
+import { RecoilRoot } from 'recoil'
+
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps<{ session: Session }>) {
+
+  return (
+    <SessionProvider session={session}>
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </SessionProvider>
+  )
 }
